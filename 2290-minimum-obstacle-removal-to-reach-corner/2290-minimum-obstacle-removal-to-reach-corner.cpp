638f8ae5955b9fd87f738888a2593ebbc7817f2f
G ARUN SAGAR <arunsagar6389@gmail.com>
2026-07-19 13:37:14 +0530

    Time: 229 ms (58.72%), Space: 103.1 MB (98.99%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+
     int minimumObstacles(vector<vector<int>>& grid) {
         vector<vector<bool>> vis(grid.size(), vector<bool>(grid[0].size(), false));
         int dx[] = {-1, 1, 0, 0};
         int dy[] = {0, 0, 1, -1};
         int ans = INT_MAX;
-        priority_queue<
-    pair<int,pair<int,int>>,
-    vector<pair<int,pair<int,int>>>,
-    greater<pair<int,pair<int,int>>>
-> q;
+       priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+        > q;
         q.push({grid[0][0],{0,0}});
         while (!q.empty()) {
 
@@ -18,7 +19,7 @@ public:
             int j = q.top().second.second;
             q.pop();
             if (i == grid.size() - 1 && j == grid[0].size() - 1) {
-                ans = min(ans, val);
+                return val;
             }
             for (int k = 0; k < 4; k++) {
                 int x = i + dx[k];
