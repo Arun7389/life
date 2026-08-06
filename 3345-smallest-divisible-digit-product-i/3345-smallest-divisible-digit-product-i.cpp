83254f1471869fbff4d984865920bbbdde88ed10
G ARUN SAGAR <arunsagar6389@gmail.com>
2026-08-06 21:35:35 +0530

    Time: 2 ms (4.28%), Space: 8.5 MB (73.4%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+
+        for (int j = n; j <= 100; j++) {
+            int i = j;
+
+            int r = 1;
+
+            while (i) {
+                r *= i % 10;
+                i /= 10;
+            }
+
+            if (r % t == 0) {
+                return j;
+            }
+        }
+        return 123456;
+    }
+};
